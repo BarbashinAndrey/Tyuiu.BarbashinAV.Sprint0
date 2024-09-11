bc17a87ad480d327b6f6eb6b4622582e1a5b994d
barey <barbashin.andrey@vk.com>
2024-09-11 22:53:48 +0500

    Task2

---
+++ Tyuiu.BarbashinAV.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.BarbashinAV.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.BarbashinAV.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Андрей"));
+            Console.ReadKey();
+        }
+    }
+}
